9d2765d1073e378c8aaee0af24d767fada6da92b
Ian Cherabier <iancher06@hotmail.fr>
2023-04-22 17:21:26 +0200

    Create Entrance of Warehouse level

--- Assets/RespawnScript.cs
+++ Assets/RespawnScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RespawnScript : MonoBehaviour
@@ -9,6 +7,13 @@ public class RespawnScript : MonoBehaviour
 
     [SerializeField] public CharacterController player;
     [SerializeField] public GameObject respawnPoint;
+
+    private void Awake() {
+        if (player == null)
+        {
+            player = FindObjectOfType<CharacterController>();
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
