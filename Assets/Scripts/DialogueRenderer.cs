43a5d914162d77805a88088d6b566f986525ec18
Luis Montaña <LuisMontana@users.noreply.github.com>
2023-05-04 12:23:27 +0200

    Added script and a prefab for dialogue renderer. It opens shortly after loading a scene but could be opened again on command.

---
+++ Assets/Scripts/DialogueRenderer.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using TMPro;
+
+public class DialogueRenderer : MonoBehaviour
+{
+    [SerializeField] private Image dialogueBackground;
+    [SerializeField] private TMP_Text textComponent;
+    [SerializeField] private float sceneLoadTextDisplayDelay;
+    private bool isDialogueEnabled = false;
+
+    private void OnEnable()
+    {
+        Invoke("ShowSceneInitialDialogue", sceneLoadTextDisplayDelay);
+    }
+
+    public void ShowSceneInitialDialogue()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        string sceneName = scene.name;
+
+        string initialDialogue = "";
+
+        Debug.Log(sceneName);
+
+        switch (sceneName)
+        {
+            case "SampleScene":
+                initialDialogue = "This is a sample scene here you can test the game \n\n Press Q to close";
+                break;
+
+            // More dialogues can be added according to which scenes they belong to.
+        }
+
+        SetText(initialDialogue);
+        ShowDialogue();
+    }
+
+    public void SetText(string newText)
+    {
+        textComponent.text = newText;
+    }
+
+    public void HideDialogue()
+    {
+        dialogueBackground.enabled = false;
+        transform.GetChild(0).gameObject.SetActive(false);
+        isDialogueEnabled = false;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogueBackground.enabled = true;
+        transform.GetChild(0).gameObject.SetActive(true);
+        isDialogueEnabled = true;
+    }
+
+    private void Update() {
+        if(isDialogueEnabled && Keyboard.current.qKey.wasPressedThisFrame) {
+            HideDialogue();
+            SetText("");
+        }
+    }
+}
