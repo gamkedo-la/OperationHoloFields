b06201a81378770fc48bf2a0a2bd7f7219e32df1
Ian Cherabier <iancher06@hotmail.fr>
2023-06-29 23:05:59 +0200

    Make Sure player is facing ahead on level start

--- Assets/Scripts/LevelStarter.cs
+++ Assets/Scripts/LevelStarter.cs
@@ -1,26 +1,52 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LevelStarter : MonoBehaviour
 {
     [SerializeField] float startFadeInSpeed = 3f; 
     private Fader fader;
 
-    private void Awake() 
+    private void Awake()
     {
         fader = FindObjectOfType<Fader>();
+        DisablePlayerInput();
     }
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
+    {
+        HideCursor();
+        InitiateFadeIn();
+
+        yield return new WaitForSeconds(startFadeInSpeed * 0.25f);
+        InablePlayerInput();
+    }
+
+    private void DisablePlayerInput()
+    {
+        InputActionAsset actions = FindObjectOfType<PlayerInput>().actions;
+        actions.FindActionMap("Player").Disable();
+        actions.FindActionMap("DeadPlayer").Enable();
+    }
+
+    private void InablePlayerInput()
+    {
+        InputActionAsset actions = FindObjectOfType<PlayerInput>().actions;
+        actions.FindActionMap("Player").Enable();
+        actions.FindActionMap("DeadPlayer").Disable();
+    }
+
+    private void InitiateFadeIn()
     {
         fader.FadeOutInstant();
         fader.SetFadingSpeed(startFadeInSpeed);
         fader.ActivateFadeIn();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideCursor()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
