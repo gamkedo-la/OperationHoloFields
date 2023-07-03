96f8e1b7534c58c4e46c87ce3b93c3fe634b7e0d
Ian Cherabier <iancher06@hotmail.fr>
2023-07-03 02:13:58 +0200

    Finishing HELL leads to corridor

--- Assets/Scripts/EndParkourLevel.cs
+++ Assets/Scripts/EndParkourLevel.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndParkourLevel : MonoBehaviour
 {
+    [SerializeField] string levelToLoad;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +19,7 @@ public class EndParkourLevel : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Player"){
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(levelToLoad);
         }
     }
 }
